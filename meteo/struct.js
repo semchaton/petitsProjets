bb0937dcb5e45c156e42308005fc98a96bbb6bee
Victor <victorcharmes.42@gmail.com>
2026-01-19 13:58:53 +0100

    Sélection ville

--- meteo/struct.js
+++ meteo/struct.js
@@ -1,4 +1,5 @@
-const url = "https://api.openweathermap.org/data/2.5/weather?q=saint-saulve&appid=dc8c9152e8adaad0ec8bf635818c0d42&units=metric";
+let ville = "Limoges";
+const url = "https://api.openweathermap.org/data/2.5/weather?q=" + ville + "&appid=dc8c9152e8adaad0ec8bf635818c0d42&units=metric";
 
 let requete = new XMLHttpRequest ();
 requete.open ('GET', url);
@@ -9,7 +10,9 @@ requete.onload = function (){
     if(requete.readyState === XMLHttpRequest.DONE){
         if(requete.status === 200){
             let reponse = requete.response;
-            console.log(reponse);
+            document.querySelector('#ville').textContent = ville;
+            document.querySelector('#temperature_label').textContent = reponse.main.temp;
+            //console.log(reponse.main.temp);
         }
         else {alert("Erreur API");}
     }
