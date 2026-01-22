f87958b34b9501e5db5ec8065e287e67f6731fbc
Victor <victorcharmes.42@gmail.com>
2026-01-22 11:13:56 +0100

    Init

---
+++ ToDoList/src/App.jsx
@@ -0,0 +1,17 @@
+function App() {
+  return (
+    <div>
+      <h1> To-Do List</h1>
+      <form>
+        <label htmlFor="todo-item"> Ajouter une chose à faire</label>
+        <input type="text" />
+        <button> Ajouter </button>
+      </form>
+      <ul>
+        
+      </ul>
+    </div>
+  )
+}
+
+export default App
